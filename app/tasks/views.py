02d37b50c61668a56003752f6e97ec33d13e3a66
Daniel Eduardo Cañón <daniel.canon@invytec.com>
2021-08-16 02:44:37 -0500

    - se añade paginación personalizada a la vista de tareas - se añade un filtro de busqueda por el campo descripción

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -1,10 +1,27 @@
 # Django rest Framework imports
 from rest_framework import viewsets, mixins
+from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from collections import OrderedDict
+from rest_framework.pagination import PageNumberPagination
 # Models and serializers
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
+
+class CustomPagination(PageNumberPagination):
+    page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('current_page', self.page.number),
+            ('pages', self.page.paginator.num_pages),
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
 
 
 class BaseViewSet(viewsets.ModelViewSet):
@@ -30,8 +47,11 @@ class BaseViewSet(viewsets.ModelViewSet):
 
 class TasksViewSet(BaseViewSet):
     """Tasks viewset"""
+    search_fields = ['description']
+    filter_backends = (filters.SearchFilter,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = CustomPagination
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
